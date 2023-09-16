b246076e09a8cd4cccfbd3da0422cc48b34cc51a
HarshTripathi29 <106900613+HarshTripathi29@users.noreply.github.com>
2023-09-16 01:36:08 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,12 @@
+// Primitive
+// Call by value (changes are made on the copy of the variables value)
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BifInt
+
+// Reference type(Non Primmitive)
+// Call by reference(changes are made on the original reference of the variable)
+// Array, Object, Function
+// Datatype of non primitives is function, object
+
+// Javascript is statically types or dynamically typed.
+
+

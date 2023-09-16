2a43159e420bcd07758018b76f73bd0dfa94dc53
HarshTripathi29 <106900613+HarshTripathi29@users.noreply.github.com>
2023-09-16 01:55:33 +0000

    datatype summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -10,3 +10,32 @@
 // Javascript is statically types or dynamically typed.
 
 
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+// Original value will not be changd in case of the primitives
+let myYoutubeName = "harsh"
+
+let anotherName = myYoutubeName
+
+anotherName = "Tripathi"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+
+// Original value will be changed in case of the non primitives
+let userOne = {
+    email : "harsh@gmail.com",
+    upi : "12345@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)

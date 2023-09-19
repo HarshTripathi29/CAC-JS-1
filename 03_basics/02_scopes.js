ae7382c72dce487c65ee97ce4c4854650181e553
HarshTripathi29 <106900613+HarshTripathi29@users.noreply.github.com>
2023-09-19 17:33:11 +0000

    sope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+console.log(c);     // This is a problem since c has a local scope and is accessible globally
+
+// Global scope is different in the windows and the node environment
+

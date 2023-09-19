b7b043001a51667369dcbcfea197fe5a510c51d4
HarshTripathi29 <106900613+HarshTripathi29@users.noreply.github.com>
2023-09-19 19:09:00 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,3 +8,37 @@ console.log(c);     // This is a problem since c has a local scope and is access
 
 // Global scope is different in the windows and the node environment
 
+function one(){
+    const username = "Hitesh"
+
+    function two()
+    {
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+// chote bache bado se ice cream mang sakte hain but bade log nahi mang sakte chote se 
+
+// +++++++++++++++++++++++++++++++ INTERESTING +++++++++++++++++++++++++++++++++++++++
+
+// Basic funtion
+
+console.log(addOne(5));
+//console.log(addTwo(60));            // gives error because variable is not initialized(Hoisting) 
+
+function addOne(num)
+{
+    return num + 1
+}
+
+// Function expression
+const addTwo = function(num){
+    return num+2
+}
+
+console.log(addTwo(6)); 
+

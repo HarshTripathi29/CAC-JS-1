a5186200d927533d98bbe3185e2725ebfe35063c
HarshTripathi29 <106900613+HarshTripathi29@users.noreply.github.com>
2023-09-20 06:07:18 +0000

    for loop break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,19 @@
+// For loop
+
+// for(let i=0; i<10; i++)
+// {
+//     const element =i;
+//     if(element==5)
+//     {
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+// Nested loop
+
+// Break and Continue keywords
+
+// Break brings the control flow out of the loop
+// Continue brings the control execution out of the current iteration
+
